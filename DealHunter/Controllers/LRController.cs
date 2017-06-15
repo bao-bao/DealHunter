b1138b36b2c6a59edabc0adb9337280a1bcb7e59
yikui <1025422395@qq.com>
2017-06-15 19:39:27 +0800

    some resource update

---
+++ DealHunter/Controllers/LRController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DealHunter.Controllers
+{
+    public class LRController : Controller
+    {
+        // GET: LR
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(int i)
+        {
+
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(int i)
+        {
+
+            return View();
+        }
+
+    }
+}
