68e1d09b4bcddf3d33dcc9c8dff88d0066e8b119
yikui <1025422395@qq.com>
2017-06-23 08:59:42 +0800

    win32.dll

---
+++ DealHunter/Models/CppDll.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Web;
+
+namespace DealHunter.Models
+{
+    public class CppDll
+    {
+        [DllImport("NumCompartordll.dll")]
+        public static extern int dhintCompare(int x, int y);
+    }
+}
